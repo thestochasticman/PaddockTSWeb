f4b3997d0a3433fbf3fa19528eb33b1fffc195fe
Yasar Adeel Ansari <yasaradeel@gmail.com>
2025-09-16 15:35:40 +0000

    eh

--- backend/main.py
+++ backend/main.py
@@ -28,7 +28,7 @@ app = FastAPI(title="Geo Viz API", version="0.2.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000",  "http://127.0.0.1:3000",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
